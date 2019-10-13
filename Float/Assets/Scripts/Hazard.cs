8e3a321e80d5ecf44e422afc9826e63483227f81
Ryan Staudacher <ryanmstaudacher@gmail.com>
2019-10-13 04:33:35 -0500

    Added Hazard script

---
+++ Float/Assets/Scripts/Hazard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
