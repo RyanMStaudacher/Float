a84c9755e77fbec624e4e0774274a17652d33a71
Ryan Staudacher <ryanmstaudacher@gmail.com>
2019-10-13 15:51:24 -0500

    Stuff

---
+++ Float/Assets/Scripts/WinDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDoor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject winText;
+
+    public bool hasWon = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasWon)
+        {
+            if(Input.GetButtonDown("Cancel"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            winText.SetActive(true);
+            hasWon = true;
+        }
+    }
+}
