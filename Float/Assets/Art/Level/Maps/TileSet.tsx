77f86ef30758c440e62ab1a4370bac764fa7a0ce
RDC088 <ryancarpenter47@yahoo.com>
2019-10-12 23:57:45 -0500

    Added Level via SuperTiled2Unity and prefab folder

--- Float/Assets/Art/Level/Maps/TileSet.tsx
+++ Float/Assets/Art/Level/Maps/TileSet.tsx
@@ -3,6 +3,9 @@
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="256" source="../TileSet/png/Tiles/Acid (1).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="1">
   <image width="256" height="256" source="../TileSet/png/Tiles/Acid (2).png"/>
@@ -42,57 +45,114 @@
  </tile>
  <tile id="13">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (1).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="14">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (2).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="15">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (3).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="16">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (4).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="17">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (5).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="18">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (6).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="19">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (7).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="20">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (8).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="21">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (9).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="22">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (10).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="23">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (11).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="24">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (12).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+   <object id="2" x="0" y="0" width="256" height="256"/>
+   <object id="3" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="25">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (13).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256"/>
+   <object id="2" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="26">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (14).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="27">
   <image width="256" height="256" source="../TileSet/png/Tiles/Tile (15).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="28">
   <image width="177" height="238" source="../TileSet/png/Objects/Barrel (1).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="177" height="238"/>
+  </objectgroup>
  </tile>
  <tile id="29">
   <image width="177" height="238" source="../TileSet/png/Objects/Barrel (2).png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="177" height="238"/>
+  </objectgroup>
  </tile>
  <tile id="30">
   <image width="256" height="256" source="../TileSet/png/Objects/Box.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="256"/>
+  </objectgroup>
  </tile>
  <tile id="31">
   <image width="280" height="464" source="../TileSet/png/Objects/DoorLocked.png"/>
